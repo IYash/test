cad6bad7c1b23044739e82e54815b7483a44a5bc
IYash <ove.nk@hotmail.com>
2018-09-17 14:04:02 +0800

    code commit

--- src/test/java/com/it/learn/UserServiceClient.java
+++ src/test/java/com/it/learn/UserServiceClient.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
  * @author huangchangling on 2017/6/13 0013
  */
 public class UserServiceClient {
-    public static final String SERVER_URL = "http://localhost:8081/router";
+    public static final String SERVER_URL = "http://localhost:8080/router";
     public static final String APP_KEY = "00001";
     public static final String APP_SECRET = "abcdefg";
     private RopClient ropClient = new DefaultRopClient(SERVER_URL,APP_KEY,APP_SECRET);
@@ -22,7 +22,7 @@ public class UserServiceClient {
         LogonRequest ropRequest = new LogonRequest();
         ropRequest.setUserName("tomson");
         //CompositeResponse response = ropClient.buildClientRequest().get(ropRequest,LogonResponse.class,"user.getSession","1.0");
-        CompositeResponse response = ropClient.buildClientRequest().post(ropRequest,LogonResponse.class,"user.getSession","1.0");
+        CompositeResponse response = ropClient.buildClientRequest().addParam("messageFormat","json").post(ropRequest,LogonResponse.class,"user.getSession","1.0");
 
         assertNotNull(response);
         assertTrue(response.isSuccessful());
